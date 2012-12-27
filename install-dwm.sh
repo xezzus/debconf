03a51a48f1a7f8029f3e0829f27f42c1708b61b2
Andrey Lisovoy <xezzus@gmail.com>
2012-12-27 17:09:57 +0400

    Correct

--- install-dwm.sh
+++ install-dwm.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-dir_dwm="~/Dwm"
-dir_workspace="~/Workspace"
-file_config="{$dir_workspace}/config.h"
+dir_dwm=~/Dwm
+dir_workspace=~/Workspace
+file_config=$dir_workspace/config.h
 
 if [ ! -d $dir_dwm ]
 then
   mkdir $dir_dwm
-fi;
+fi
 
 cd $dir_dwm && apt-get source dwm && cd dwm-* && cp $file_config .
