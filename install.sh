19b9b008b4473449c2fa71b7fdc936a13121a28f
Andrey Lisovoy <xezzus@gmail.com>
2012-12-27 21:39:43 +0400

    Correct

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aptitude install git vim-gtk dpkg-dev dwm suckless-tools
+aptitude install git vim-gtk dpkg-dev dwm suckless-tools xclip
 
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
