d4d7f811fff1a7359fbab135480ae06aa14733f4
Andrey Lisovoy <xezzus@gmail.com>
2012-12-27 16:36:49 +0400

    Correct

---
+++ install-dwm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd && mkdir Dwm && cd Dwm && apt-get source dwm
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 aptitude install git vim-gtk dpkg-dev dwm suckless-tools
-cd && mkdir Dwm && cd Dwm && apt-get source dwm
 
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
