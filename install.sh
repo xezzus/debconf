87ffd9b2c5ac7628f63b5a40ecc72546f42b623f
Andrey Lisovoy <xezzus@gmail.com>
2013-01-04 16:54:12 +0400

    correct

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-aptitude install git vim-gtk dpkg-dev dwm suckless-tools xclip
-
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
+aptitude install git vim-gtk dpkg-dev dwm suckless-tools xclip mongodb-10gen
