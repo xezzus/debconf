363bd7701a89eff31e0e3f598881541ae1777a87
Andrey Lisovoy <xezzus@gmail.com>
2013-01-05 20:08:42 +0400

    Correct

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
-aptitude install sudo git vim-gtk dpkg-dev dwm suckless-tools xclip mongodb-10gen
+aptitude install sudo git vim-gtk dpkg-dev dwm suckless-tools xclip mongodb-10gen xserver-xorg-core xorg
 
 cp sudoers /etc
+cp xinitrc /home/web/.xinitrc
 
 
