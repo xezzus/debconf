f17d89b11e13e9dc5a92b69604c68f11c7e3e899
Andrey Lisovoy <xezzus@gmail.com>
2013-01-05 22:56:26 +0400

    Correct

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 user=web
 home=/home/$user
 dwm=~/Dwm
@@ -12,21 +11,30 @@ cat dotdeb.gpg | apt-key add -
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 gpg --keyserver subkeys.pgp.net --recv A040830F7FAC5991 && gpg --export --armor A040830F7FAC5991 | sudo apt-key add -
 aptitude update && aptitude full-upgrade
-aptitude install sudo git vim-gtk dpkg-dev dwm suckless-tools xclip mongodb-10gen xserver-xorg-core xorg sakura google-chrome-stable
+aptitude install sudo git vim-gtk dpkg-dev dwm suckless-tools xclip mongodb-10gen xserver-xorg-core xorg sakura google-chrome-stable nginx
 
 # INSTALL DWM
-if [ ! -d $dwm ]
-then
-  mkdir $dwm
-fi
+mkdir -p $dwm
 cd $dwm && apt-get source dwm && cd dwm-* && cp $dir/config.h .
 apt-get build-dep dwm
 cd $dwm/dwm*
 make clean install
 
-# INSTALL CONFIG
-cp ~/Workspace/sudoers /etc
-cp ~/Workspace/xinitrc $home/.xinitrc
-cp ~/Workspace/sakura.conf $home/.config/sakura
-chown -R $user:$user $home/*
+# INSTALL NGINX
+cp $dir/nginx.conf /etc/nginx
+mkdir -p $home/public/use/mysql/public
+mkdir -p $home/public/use/pgadmin/public
+mkdir -p $home/public/use/mongo/public
+mkdir -p $home/public/use/mongo/public
+mkdir -p $home/public/use/joomla/public
+mkdir -p $home/public/use/ocstore/public
+mkdir -p $home/public/use/opencart/public
+mkdir -p $home/public/use/oscommerce/public
+mkdir -p $home/public/use/wordpress/public
+mkdir -p $home/public/webcmf/public
 
+# INSTALL CONFIG
+cp $dir/sudoers /etc
+cp $dir/xinitrc $home/.xinitrc
+cp $dir/sakura.conf $home/.config/sakura
+chown -R $user:$user $home/*
