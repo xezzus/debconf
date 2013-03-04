89caece1ced3913cc0481d81af7c00ea00dbe517
xezzus <xezzus@gmail.com>
2013-03-04 07:28:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,9 @@ wget http://www.dotdeb.org/dotdeb.gpg && cat dotdeb.gpg | apt-key add -
 # KEY GOOGLE
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
+# KEY POSTGRES
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
 # UPDATE
 aptitude update && aptitude full-upgrade
 
